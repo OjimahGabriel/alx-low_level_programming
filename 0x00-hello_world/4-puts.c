1ea9eb4199d4113d5cd05024b0c13b55ce700d87
Ojimah Gabriel <ojimahgabriel2@gmail.com>
2022-03-12 03:09:35 -0800

    moving

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * Description- Print out a string out with double quote
+ *
+ * Return: Always 0(success)
+ *
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
